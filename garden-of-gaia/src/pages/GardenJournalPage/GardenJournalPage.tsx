55dfedafab94ed275347cffb178b9927139da229
candrews42 <andrews.colinj@gmail.com>
2023-12-12 16:12:20 +0300

    added filtering

--- garden-of-gaia/src/pages/GardenJournalPage/GardenJournalPage.tsx
+++ garden-of-gaia/src/pages/GardenJournalPage/GardenJournalPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Container } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, TextField, Container } from '@mui/material';
 import fetchGardenJournalData from '../../utils/fetchGardenJournalData';
 
 // Define a type for the entries
@@ -19,25 +19,33 @@ type EntryType = {
 const GardenJournalPage: React.FC = () => {
     const [gardenJournal, setGardenJournal] = useState<EntryType[]>([]);
     const [plantTracker, setPlantTracker] = useState<EntryType[]>([]);
+    const [filter, setFilter] = useState(''); // State variable for the filter input
+    const [tempFilter, setTempFilter] = useState(''); // Temporary state for the input value
 
     useEffect(() => {
         fetchGardenJournalData(`${process.env.REACT_APP_SERVER_API_URL}/api/observations`, setGardenJournal);
         fetchGardenJournalData(`${process.env.REACT_APP_SERVER_API_URL}/api/plant_tracker`, setPlantTracker);
     }, []);
 
-    // Change 'date' to 'datetime' in plantTracker
-    const new_gardenJournal = gardenJournal.map(entry => ({
-        ...entry,
-        datetime: entry.date,
-        location_name: entry.location
-    }));
+    // Function to handle Enter key press
+    const handleKeyPress = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            setFilter(tempFilter);
+        }
+    };
 
     // Combine both datasets and sort by date
-    const combinedData = [...new_gardenJournal, ...plantTracker].sort((a, b) => {
+    const combinedData = [...gardenJournal, ...plantTracker]
+
+    // Apply the filter only if filter string is not empty
+    const filteredData = tempFilter 
+        ? plantTracker.filter(entry => entry.plant_name?.toLowerCase() === tempFilter.toLowerCase())
+        : combinedData;
+
+    filteredData.sort((a, b) => {
         return new Date(b.date).getTime() - new Date(a.date).getTime();
     });
 
-    // Check if an entry is from plantTracker
     const isPlantTrackerEntry = (entry: EntryType) => entry.plant_name || entry.action_category;
 
     return (
@@ -45,6 +53,14 @@ const GardenJournalPage: React.FC = () => {
             <Typography variant="h3" gutterBottom component="div">
                 Garden Journal
             </Typography>
+            <TextField
+                label="Filter by Plant Name"
+                variant="outlined"
+                value={tempFilter}
+                onChange={(e) => setTempFilter(e.target.value)}
+                onKeyPress={handleKeyPress}
+                sx={{ marginBottom: 2 }}
+            />
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
@@ -57,10 +73,10 @@ const GardenJournalPage: React.FC = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {combinedData.map((entry) => (
+                        {filteredData.map((entry) => (
                             <TableRow key={entry.id} style={isPlantTrackerEntry(entry) ? { fontStyle: 'italic', backgroundColor: '#e8f5e9' } : {}}>
-                                <TableCell>{entry.datetime}</TableCell>
-                                <TableCell>{entry.location}</TableCell>
+                                <TableCell>{entry.date}</TableCell>
+                                <TableCell>{entry.location_id}</TableCell>
                                 <TableCell>{entry.plant_name || entry.username || ''}</TableCell>
                                 <TableCell>{entry.action_category || entry.current_location || ''}</TableCell>
                                 <TableCell>{entry.notes}</TableCell>
